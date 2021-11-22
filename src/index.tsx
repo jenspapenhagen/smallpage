11be2e2132cf70739442d418cfc462a601f52342
Jens <jens.papenhagen@web.de>
2021-11-22 19:57:44 +0100

    make it less files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FetchComponent } from "./FetchComponent";
+import { ItemList } from "./itemList";
+import voicesData from './data.json';
 
-const App = () => (
-  <FetchComponent />
+const App = () => (<>
+      <div className="center"><img src="./img/toucan-281491_1280.webp"  width="250" /></div>
+      <ItemList records={voicesData.recordings} />
+      <br />
+      <h5>Data form: <a href="https://www.xeno-canto.org" target="_blank" rel="noreferrer"> xeno-canto</a></h5>
+      <h5>Image form: <a href="https://pixabay.com/de/photos/tukan-vogel-dschungel-zoo-exotisch-281491/" target="_blank" rel="noreferrer"> Pixabay License</a></h5>
+      <h5>Code here: <a href="https://github.com/jenspapenhagen/smallpage/">gitHub</a></h5>
+    </>
 );
 
 ReactDOM.render(
