4803997e2d8c277b0bc719c24b8f2aed9f984c48
Jens <jens.papenhagen@web.de>
2021-11-24 17:57:06 +0100

    build cheesy revert proxy in php for bypassing CORS on remote Endpoint

---
+++ reversproxy.php
@@ -0,0 +1,32 @@
+<?php
+//from: https://github.com/mustangostang/spyc
+require_once "spyc.php";
+
+//load yml file for config
+$file = 'config.yml';
+$data = Spyc::YAMLLoad($file);
+if ($data === NULL || $data === false) {
+    throw new WFException("Error processing YAML file: {$file}");
+}
+//building URL for the target
+$protocol = $data['backend']['protocol'];
+$domain = $data['backend']['domain'];
+$path = $data['backend']['path'];
+$parameter = $data['parameter'];
+
+$fullURL = $protocol . '://'. $domain .'/'.  $path . '?'. $data['parameter'][0];
+//https://www.xeno-canto.org/api/2/recordings?query=Keel-billed+Toucan
+
+//get input form react frontend
+$firstParameterAsString = $data['parameter'][0];
+if ($firstParameterAsString === NULL){
+    throw new WFException("Error processing GET Parameter");
+}
+$q = urlencode($_GET[$firstParameterAsString]);//Need to url encode
+
+//call target and return the responce als JSON
+$response = file_get_contents($fullURL .'=' .$q);
+
+//Return the response back to AJAX, assuming it is already returned as JSON.
+// Else encode it json_encode($response)
+echo $response; 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { ItemList } from './itemList';
-import voicesData from './data.json';
+import { Birdvoices } from './api/parser';
 
-const App = () => (<>
+const App = () => {
+    const [voicesData, setVoicesData] = useState<Birdvoices>();
+    useEffect(() => {
+        fetch('reversproxy.php?query=Keel-billed+Toucan', {
+                method: 'GET',
+                mode: 'no-cors',
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                  },
+            })
+            .then(response => response.json())
+            .then(data => setVoicesData(data));
+    }, []);
+
+    return voicesData ? (<>
         <div className='center'></div>
         <ItemList records={voicesData.recordings} />
         <br />
@@ -12,7 +27,7 @@ const App = () => (<>
                            target='_blank' rel='noreferrer'> Pixabay License</a></h5>
         <h5>Code here: <a href='https://github.com/jenspapenhagen/smallpage/'>gitHub</a></h5>
     </>
-);
+): (<><p>Loading ...</p></>)};
 
 ReactDOM.render(
     <React.StrictMode>
