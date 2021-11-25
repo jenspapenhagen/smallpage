a58a2b5e5577ecee71fadd1853847505aa8435ec
Jens <jens.papenhagen@web.de>
2021-11-25 17:38:14 +0100

    adding SEO and a spinner for page loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import { ItemList } from './itemList';
-import { Birdvoices } from './api/parser';
+import {ItemList} from './itemList';
+import {Birdvoices} from './api/parser';
 
 const App = () => {
     const [voicesData, setVoicesData] = useState<Birdvoices>();
+
     useEffect(() => {
         fetch('reverseproxy/?query=Keel-billed+Toucan', {
-                method: 'GET',
-                mode: 'no-cors',
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json"
-                  },
-            })
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            },
+        })
             .then(response => response.json())
-            .then(data => setVoicesData(data));
+            .then(data => setVoicesData(data))
     }, []);
 
-    return voicesData ? (<>
-        <div className='center'></div>
-        <ItemList records={voicesData.recordings} />
-        <br />
-        <h5>Data form: <a href='https://www.xeno-canto.org' target='_blank' rel='noreferrer'> xeno-canto</a></h5>
-        <h5>Image form: <a href='https://pixabay.com/de/photos/tukan-vogel-dschungel-zoo-exotisch-281491/'
-                           target='_blank' rel='noreferrer'> Pixabay License</a></h5>
-        <h5>Code here: <a href='https://github.com/jenspapenhagen/smallpage/'>gitHub</a></h5>
-    </>
-): (<><p>Loading ...</p></>)};
+    return !voicesData ? <div className='loader'>Loading...</div> : (<>
+            <div className='center'></div>
+            <ItemList records={voicesData.recordings}/>
+            <br/>
+            <h5>Data form: <a href='https://www.xeno-canto.org' target='_blank' rel='noreferrer'> xeno-canto</a></h5>
+            <h5>Image form: <a href='https://pixabay.com/de/photos/tukan-vogel-dschungel-zoo-exotisch-281491/'
+                               target='_blank' rel='noreferrer'> Pixabay License</a></h5>
+            <h5>Code here: <a href='https://github.com/jenspapenhagen/smallpage/'>gitHub</a></h5>
+        </>
+    )
+};
 
 ReactDOM.render(
     <React.StrictMode>
